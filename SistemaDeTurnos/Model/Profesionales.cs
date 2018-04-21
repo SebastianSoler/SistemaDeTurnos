83293de120c6737aaf0415eb6a967a88664bdc04
SebastianSoler <SebastianSoler@users.noreply.github.com>
2018-04-20 23:20:17 -0300

    Inic Project
    Primer Commit, Proyecto inicial

---
+++ SistemaDeTurnos/Model/Profesionales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Turnero.Model
+{
+    public class Profesionales
+    {
+        //Attributes
+        public string IdProfecional { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+    }
+}
