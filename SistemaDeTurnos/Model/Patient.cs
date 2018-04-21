0ba0bf1584ec2780e71b2071bf3cf741d8f5eaf9
SebastianSoler <SebastianSoler@users.noreply.github.com>
2018-04-21 00:33:58 -0300

    Initial :: Create Projects

---
+++ SistemaDeTurnos/Model/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeTurnos.Model
+{
+    public class Patient
+    {
+        public string IdPatient { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurnName { get; set; }
+    }
+}
---
+++ SistemaDeTurnos/Model/Professional.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Turnero.Model
+{
+    public class Professional
+    {
+        //Attributes
+        public string IdProfetional { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName{ get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string  Address { get; set; }
+
+        public string Telphone { get; set; }
+    }
+}
